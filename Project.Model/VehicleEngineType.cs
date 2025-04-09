6a6c5f70391793243d4d2bf4a7f342af85c4fa27
dmgoj <domagoj.grgic@outlook.com>
2025-04-09 23:53:19 +0200

    feat: add Project.Model project with implementation of model interfaces

---
+++ Project.Model/VehicleEngineType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Models.Common;
+
+namespace Project.Model
+{
+    public class VehicleEngineType : IVehicleEngineType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Abrv { get; set; }
+    }
+}
---
+++ Project.Model/VehicleRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Models.Common;
+
+namespace Project.Model
+{
+    public class VehicleRegistration : IVehicleRegistration
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+    {
+    }
+}
