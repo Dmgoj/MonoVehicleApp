5737db51ab6bfcf1f9e4c0ec487dfc8aa23df167
dmgoj <domagoj.grgic@outlook.com>
2025-04-10 12:24:57 +0200

    feat: Add project Project.Repository.Common containing interfaces for generic Repository, UnitOfWork repository and Entity-Specific repositories

---
+++ Project.Repository.Common/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Project.Repository.Common
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        Task<TEntity> GetByID(object id);
+
+        Task Insert(TEntity entity);
+
+        Task Delete(object id);
+
+        Task Delete(TEntity entityToDelete);
+
+        Task Update(TEntity entityToUpdate);
+    }
+}
