6663ac6caf16cd44d419aef5aff2c17c49d402ee
dmgoj <domagoj.grgic@outlook.com>
2025-04-23 22:55:56 +0200

    feat: add service interfaces for VehicleMake, VehicleModel, VehicleOwner, VehicleRegistration and VehicleEngineType

---
+++ Project.Service.Common/IVehicleModelService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Service.Common.Parameters;
+
+namespace Project.Service.Common
+{
+    public interface IVehicleModelService
+    {
+        Task<PaginatedResult<VehicleModelDto>> GetAllAsync(QueryParameters parameters);
+        Task<VehicleModelDto> GetByIdAsync(int id);
+        Task<VehicleModelDto> CreateAsync(VehicleModelForCreateDto dto);
+        Task UpdateAsync(int id, VehicleModelForUpdateDto dto);
+        Task DeleteAsync(int id);
+    }
+}
