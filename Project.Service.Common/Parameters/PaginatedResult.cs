4d26efa49c619cf60a72384f405b7e7fcf9c8891
dmgoj <domagoj.grgic@outlook.com>
2025-04-23 22:53:37 +0200

    feat: add QueryParameters and PaginatedResult<T> for filtering, sorting and paging

---
+++ Project.Service.Common/Parameters/PaginatedResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Common.Parameters
+{
+    public class PaginatedResult<T>
+    {
+        public IEnumerable<T> Items { get; init; }
+        public int TotalCount { get; init; }
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+
+        public bool HasNext => PageNumber * PageSize < TotalCount;
+        public bool HasPrevious => PageNumber > 1;
+    }
+}
---
+++ Project.Service.Common/Parameters/QueryParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Common;
+
+namespace Project.Service.Common.Parameters
+{
+    public class QueryParameters : PagingParameters
+    {
+        public string Filter { get; set; }
+        public string SortBy { get; set; }
+        public bool SortDescending { get; set; }
+    }
+}
