315c6ed8bf842e58d489a63cf0349de59afc6739
dmgoj <domagoj.grgic@outlook.com>
2025-04-29 23:55:45 +0200

    chore: add “using Project.Service.Common.Parameters” to IVehicleMakeService

--- Project.Service.Common/IVehicleMakeService.cs
+++ Project.Service.Common/IVehicleMakeService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.Service.Common.Parameters;
 
 namespace Project.Service.Common
 {
