8f7b3af74c17efc6e9868d53d99d66c61f9d8978
dmgoj <domagoj.grgic@outlook.com>
2025-04-29 23:48:28 +0200

    refactor: change property name EngineTypeName to EngineType

--- Project.Service.Common/DTOs/VehicleRegistrationDto.cs
+++ Project.Service.Common/DTOs/VehicleRegistrationDto.cs
@@ -5,7 +5,7 @@
     public int ModelId { get; init; }
     public string ModelName { get; init; }
     public int EngineTypeId { get; init; }
-    public string EngineTypeName { get; init; }
+    public string EngineType { get; init; }
     public int OwnerId { get; init; }
     public string OwnerFullName { get; init; }
 }
