e0f6e27107cc67ecc0ae77d7b172484ae8288b17
dmgoj <domagoj.grgic@outlook.com>
2025-04-29 23:50:54 +0200

    feat: add Vehicle…SortField enums for SortBy property

--- Project.Service.Common/Parameters/QueryParameters.cs
+++ Project.Service.Common/Parameters/QueryParameters.cs
@@ -7,6 +7,39 @@ using Project.Common;
 
 namespace Project.Service.Common.Parameters
 {
+    #region VehicleSortByFields
+    public enum VehicleMakeSortField
+    {
+        Name,
+        Abrv
+    }
+
+    public enum VehicleModelSortField
+    {
+        Name,
+        Abrv
+    }
+
+    public enum VehicleOwnerSortField
+    {
+        FirstName,
+        LastName,
+        DOB
+    }
+
+    public enum VehicleEngineTypeSortField
+    {
+        Type
+    }
+
+    public enum VehicleRegistrationSortField
+    {
+        RegistrationNumber,
+        VehicleModel,
+        VehicleEngineType,
+        VehicleOwner
+    }
+    #endregion
     public class QueryParameters : PagingParameters
     {
         public string Filter { get; set; }
