19c211fa040584e1fa5cf59ce3f7a90210dfdd86
dmgoj <domagoj.grgic@outlook.com>
2025-05-07 02:15:35 +0200

    feat(vehicle-owner-dto): replace VehicleModel/Make fields with Cars collection

--- Project.Service.Common/DTOs/VehicleOwnerDto.cs
+++ Project.Service.Common/DTOs/VehicleOwnerDto.cs
@@ -4,8 +4,5 @@
     public string FirstName { get; init; }
     public string LastName { get; init; }
     public DateTime DOB { get; init; }
-    public int? VehicleModelId { get; init; }
-    public string VehicleModel { get; init; }
-    public int? VehicleMakeId { get; init; }
-    public string VehicleMake { get; init; }
+    public List<CarDto> Cars { get; init; }
 }
